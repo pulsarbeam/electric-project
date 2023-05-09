15b99afd09b2ced7ef1749b5ba3fceaa22bf7eb8
Rene Groothuis <renegrooth@gmail.com>
2023-05-09 17:31:49 +1200

    added some styling

--- electric-app/src/components/Form.tsx
+++ electric-app/src/components/Form.tsx
@@ -30,12 +30,17 @@ function Form() {
   }
 
   return (
-    <div className="rounded-div">
+    <div className="flex flex-col justify-center rounded-div">
       <form>
         <FormInputs data={formData} handleChange={handleChange} />
       </form>
-      <div>
-        <button onClick={handleSubmit}>Submit</button>
+      <div className="flex justify-center">
+        <button
+          className="px-6 py-2 my-8 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
       </div>
     </div>
   )
--- electric-app/src/components/FormInputs.tsx
+++ electric-app/src/components/FormInputs.tsx
@@ -1,66 +1,75 @@
 import { Props } from '../models'
 
-// interface FormInputsProps {
-//   formData: {
-//     name: string,
-//     email: string,
-//     phone: string
-//   };
-//   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-// }
-
 const FormInputs: React.FC<Props> = ({ data, handleChange }) => {
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter Referance Number"
-        value={data.ref}
-        onChange={handleChange}
-        name="ref"
-      />
-      <input
-        type="text"
-        placeholder="Enter Client Location"
-        value={data.location}
-        onChange={handleChange}
-        name="location"
-      />
-      <input
-        type="text"
-        placeholder="Enter Clients Contact Details"
-        value={data.contact}
-        onChange={handleChange}
-        name="contact"
-      />
-      <input
-        type="text"
-        placeholder="Enter Electricans Name"
-        value={data.worker}
-        onChange={handleChange}
-        name="worker"
-      />
-      <input
-        type="text"
-        placeholder="Enter Electricians Registration Number"
-        value={data.regNum}
-        onChange={handleChange}
-        name="regNum"
-      />
-      <input
-        type="text"
-        placeholder="Enter Electricians Phone Number"
-        value={data.phoneNum}
-        onChange={handleChange}
-        name="phoneNum"
-      />
-      <input
-        type="text"
-        placeholder="Enter Electricians Email Address"
-        value={data.email}
-        onChange={handleChange}
-        name="email"
-      />
+    <div className="rounded-lg bg-white p-10">
+      <form>
+        <div className="grid grid-cols-1 gap-8">
+          <input
+            className="border border-gray-300 rounded-lg p-2 text-left w-full"
+            type="text"
+            placeholder="Enter Reference Number"
+            value={data.ref}
+            onChange={handleChange}
+            name="ref"
+          />
+
+          <div className="grid grid-cols-2 gap-8">
+            <input
+              className="border border-gray-300 rounded-lg p-2 text-left w-full"
+              type="text"
+              placeholder="Client Location"
+              value={data.location}
+              onChange={handleChange}
+              name="location"
+            />
+            <input
+              className="border border-gray-300 rounded-lg p-2 text-left w-full"
+              type="text"
+              placeholder="Client's Contact Details"
+              value={data.contact}
+              onChange={handleChange}
+              name="contact"
+            />
+          </div>
+
+          <input
+            className="border border-gray-300 rounded-lg p-2 text-left w-full"
+            type="text"
+            placeholder="Electrician's Name"
+            value={data.worker}
+            onChange={handleChange}
+            name="worker"
+          />
+
+          <input
+            className="border border-gray-300 rounded-lg p-2 text-left w-full"
+            type="text"
+            placeholder="Enter Electrician's Registration Number"
+            value={data.regNum}
+            onChange={handleChange}
+            name="regNum"
+          />
+
+          <input
+            className="border border-gray-300 rounded-lg p-2 text-left w-full"
+            type="text"
+            placeholder="Electrician's Phone Number"
+            value={data.phoneNum}
+            onChange={handleChange}
+            name="phoneNum"
+          />
+
+          <input
+            className="border border-gray-300 rounded-lg p-2 text-left w-full"
+            type="text"
+            placeholder="Enter Electrician's Email Address"
+            value={data.email}
+            onChange={handleChange}
+            name="email"
+          />
+        </div>
+      </form>
     </div>
   )
 }
